d467764ef199b4dba3ff6211ff1e591853981530
Ramon <ac2262ra0530@pusd.us>
2015-02-05 15:02:41 -0800

    xh

--- controller/control-dp.php
+++ controller/control-dp.php
@@ -1,5 +1,13 @@
 <?php
-   require_once("../model/database.php");
+   require_once(__DIR__ . "../model/database.php");
    
    $connection = new myslqi($host, $usename, $password );
-
+   
+   if($connection->connect_error) {
+       die("error: " . $connection->connect_error);
+   }
+   else {
+       echo "success" . $connection->host_info;
+   }
+   
+   $connection->close();
